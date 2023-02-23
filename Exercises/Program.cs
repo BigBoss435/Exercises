a99624e3d3309bf7db2d0f65d61b909e2bd9c6ce
BigBoss435 <deividas.kalvelis@gmail.com>
2023-02-23 17:30:49 +0200

    2 exercises

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -6,7 +6,29 @@ namespace Exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(AddAndMultiply(5, 4, 3));
+            Console.WriteLine(CelsiusToFar(-30));
+        }
+
+        //Add and multiply exercise
+        public static double AddAndMultiply(int x, int y, int z)
+        {
+            return (x + y) * z;
+        }
+
+        //Celsius to farenheit exercise
+        public static string CelsiusToFar(double celsius)
+        {
+            double farenheit = 0;
+            if (celsius > -271.15)
+            {
+                farenheit = celsius * 1.8 + 32;
+                return $"T = {farenheit}F";
+            }
+            else
+            {
+                return "Temperature does not exist";
+            }
         }
     }
 }
